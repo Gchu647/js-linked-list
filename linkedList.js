5b819428ab1b5a0a08361a412f4862d38c60ddec
George <quicksilver-ga@hotmail.com>
2018-07-05 15:37:57 -1000

    Made an initial add and getHead method

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,25 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  let head = null;
+  let tail = null;
 
+  function getHead() {
+    return head;
+  }
+
+  function add(val, nextObj) {
+    var makeNode = {};
+    makeNode.value = val;
+    makeNode.next = nextObj; //default should be null
+
+    head = makeNode; // We are storing it in head for now...
+  }
+
+  //Testing happening down here
+  console.log(getHead());
+  add("ready Player", null);
+  console.log(getHead());
 }
+
+linkedListGenerator();
