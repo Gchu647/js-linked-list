9dc449c85e5935a9f28189d4b2fc69b46c7b6ac7
George <quicksilver-ga@hotmail.com>
2018-07-05 22:57:47 -1000

    Find function works, but console.log two times

--- linkedList.js
+++ linkedList.js
@@ -9,7 +9,8 @@ function linkedListGenerator(){
   let oldTail = null;
 
   function getHead() {
-    console.log(head); //Checkout everything inside the head, needs to be modified so it doesn't see next.
+    return head;
+    //console.log(head);
   }
 
   function getTail() {
@@ -32,14 +33,44 @@ function linkedListGenerator(){
     if(!head) {
       head = currentTail; // Keep tracks of what is the head
     }
+
+    // console.log(currentTail);
   }
 
+  function get(num) {
+    let counter = 1;
+    let currentNode = getHead();
+    console.log("it works!");
+
+    while(currentNode.next !== null && typeof num === "number") {
+      console.log("loop: ", counter);     
+
+      if(counter === num) {
+        console.log(currentNode.value);
+        break;
+      } else {
+        currentNode = currentNode.next;
+        counter ++;
+      }
+    }
+
+    console.log(currentNode.value);
+  }
+
+  /*
   return {
     getHead: getHead,
     getTail: getTail,
-    add: add
+    add: add,
+    get: get
   }
+  */
+
   //Testing happening down here
+  add("Player One");
+  add("Player Two");
+  add("Player Three");
+  get(2);
 }
 
 linkedListGenerator();
