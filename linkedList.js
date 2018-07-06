da4ce1792cfdcb6cadfaf0b72f46477e3db7bdbc
George <quicksilver-ga@hotmail.com>
2018-07-05 16:49:01 -1000

    An inefficient add function that can make a nodelist greater than three

--- linkedList.js
+++ linkedList.js
@@ -6,23 +6,37 @@
 function linkedListGenerator(){
   let head = null;
   let tail = null;
+  let prevNode = null;
 
   function getHead() {
-    return head;
+    console.log(head); //Checkout everything inside the head, needs to be modified so it doesn't see next.
   }
 
-  function add(val, nextObj) {
+  function add(val) {
     var makeNode = {};
     makeNode.value = val;
-    makeNode.next = nextObj; //default should be null
 
-    head = makeNode; // We are storing it in head for now...
+    if(!head) { // going to have to use the function again for tail
+      makeNode.next = null;
+      head = makeNode;
+      prevNode = head;
+    } else {
+      makeNode.next = null;
+      prevNode.next = makeNode; // makeNode has to change the "next" property of the prevNode
+      prevNode = makeNode; // The obj of the prevNode also has to be change so Node 1 doesn't point to Node 3
+    }
+
+    console.log(makeNode); // Return the objects being added
   }
 
   //Testing happening down here
-  console.log(getHead());
-  add("ready Player", null);
-  console.log(getHead());
+  add("ready Player one");
+  add("Player two");
+  add("Player Three");
+  add("player four");
+  add("player five");
+  debugger;
+  getHead();
 }
 
 linkedListGenerator();
